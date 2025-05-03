454266f61a148fef7a21155d32435ae2bd0cb493
Xavier <xavier.d.salazar5@gmail.com>
2025-05-03 14:43:27 -0400

    Added flags for main function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from db.database_helper import Database
 from email_parser import parse_emails
 from report_generator import generate_report
@@ -15,36 +16,45 @@ def setup_database():
 # Main function entry point
 
 def main():
+    # Introduction
+    print("Welecome to Xavier's personal FINANCIAL TRAKCER!")
+    
+    parser = argparse.ArgumentParser(description="Xavier's Personal Financial Tracker")
+    parser.add_argument('-p','--parse-only', action='store_true', help='Only parse emails, do not generate a report')
+    parser.add_argument('-r', '--report-only', action='store_true', help='Only generate a report, do not parse emails')
+    args = parser.parse_args()
+
     """ Main function for application workflow """
+ 
     # Step 1: Setup database for first time users
     setup_database()
 
     # Step 2: Parse emails to collect transaction data
-    print("Running email parser...")
-    parse_emails(use_sample_emails=False)
-
-    # Step 3: Generate desired report
-    print("Welecome to Xavier's personal FINANCIAL TRAKCER!")
+    if not args.report_only:
+        print("Running email parser...")
+        parse_emails(use_sample_emails=False)
     
-    try:
-        # Prompt user for month and year for desired report
-        month = int(input("Enter the month (1-12) for the report: "))
-        year = int(input("Enter the year (e.g., 2024) for the report: "))
-        # Check for valid inputs
-        if not (1 <= month <= 12):
-            raise ValueError("Month must be between 1 and 12.")
-        
-        date_str = f"{datetime(year, month, 1).strftime('%B')} 1, {year}"
-        print("Generating report...")
+    if not args.parse_only:
+       
+        try:
+            # Prompt user for month and year for desired report
+            month = int(input("Enter the month (1-12) for the report: "))
+            year = int(input("Enter the year (e.g., 2024) for the report: "))
+            # Check for valid inputs
+            if not (1 <= month <= 12):
+                raise ValueError("Month must be between 1 and 12.")
+            
+            date_str = f"{datetime(year, month, 1).strftime('%B')} 1, {year}"
+            print("Generating report...")
 
-        generate_report(date_str)
+            generate_report(date_str)
 
-    except ValueError as ve:
-        print(f"Invalid input: {ve}")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+        except ValueError as ve:
+            print(f"Invalid input: {ve}")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
 
-    print("Process completed.")
+        print("Process completed.")
 
 if __name__ == "__main__":
     main()
